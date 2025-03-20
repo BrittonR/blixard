0b133dbb799dd090b6329ed47309a8e20cde33ef
brittonr <b@robitzs.ch>
2025-03-20 16:17:29 -0600

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,9 +1,7 @@
 {
   description = "A Nix-flake-based Gleam development environment";
-
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
   
-
   outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
@@ -15,6 +13,14 @@
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [ gleam erlang rebar3 elixir];
+          
+          # Add environment variables for Khepri configuration
+          shellHook = ''
+  export BLIXARD_STORAGE_MODE="ets_dets"
+  export BLIXARD_ETSDETS_OPS="start,stop,get_vm,get_host,list_vms,list_hosts"
+  echo "BLIXARD_STORAGE_MODE set to: $BLIXARD_STORAGE_MODE"
+  echo "BLIXARD_ETSDETS_OPS set to: $BLIXARD_ETSDETS_OPS"
+'';
         };
       });
     };
